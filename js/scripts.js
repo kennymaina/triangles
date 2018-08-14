3f3e4d84957192fcf7e4f966086895016231312c
kennymaina <victormurigi@outlook.com>
2018-08-14 06:58:03 +0300

    changed header color in styles.css

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var triangleTracker = function() {
+var a = parseFloat(prompt("enter the length"));
+var b = parseFloat(prompt("enter the height"));
+var c = parseFloat(prompt("enter the hypo"));
+if(isNan(a)===true || isNan(b)===true || isNan(c)===true || a<=0 || b<=0 || c<=0) {
+  alert("enter a correct number")
+}
+else if((a + b) <=c || (b + c) <=a || (a + c) <=b) {
+  alert("not a triangle");
+}
+else if(a===b && b===c) {
+  alert("this is an equilateral");
+}
+else if(a===b || b===c || a===c) {
+  alert("this is an isosceles triangle");
+}else{
+  alert(scalene);
+}
+}
+triangleTracker();

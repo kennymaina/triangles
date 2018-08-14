049c8d263f4721f41bb3d8298df16e89ba545fcb
kennymaina <victormurigi@outlook.com>
2018-08-14 09:31:33 +0300

    addition of mats a triangle in images

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var triangleTracker = function() {
 var a = parseFloat(prompt("enter the length"));
 var b = parseFloat(prompt("enter the height"));
 var c = parseFloat(prompt("enter the hypo"));
-if(isNan(a)===true || isNan(b)===true || isNan(c)===true || a<=0 || b<=0 || c<=0) {
+if(isNaN(a)===true || isNaN(b)===true || isNaN(c)===true || a<=0 || b<=0 || c<=0) {
   alert("enter a correct number")
 }
 else if((a + b) <=c || (b + c) <=a || (a + c) <=b) {
@@ -17,10 +17,3 @@ else if(a===b || b===c || a===c) {
   alert(scalene);
 }
 }
-triangleTracker();
-
-var Triangles = [
-    "equilateral",
-    "isosceles",
-    "scalene"
-];
